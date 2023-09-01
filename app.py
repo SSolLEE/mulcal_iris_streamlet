06d623bf7a2f0bf39901fddac0226d0c60949670
Sol LEE <datasoling@gmail.com>
2023-09-01 10:00:08 +0900

    update

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import sklearn as sk
+import plotly.plotly as py
 
 
 def main():
@@ -12,6 +14,10 @@ def main():
     st.markdown("# Hello World")
     st.write(np.__version__)
     st.write(pd.__version__)
+    st.write(plt.__version__)
+    st.write(sns.__version__)
+    st.write(sk.__version__)
+    st.write(py.__version__)
 
 if __name__ == "__main__":
         main()
