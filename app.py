6aa182133eac8de0962e7380abe0017a37805a38
Sol LEE <datasoling@gmail.com>
2023-09-01 09:44:14 +0900

    update

--- app.py
+++ app.py
@@ -2,10 +2,13 @@
 
 import streamlit as st
 import numpy as np
+import pandas as pd
+
 def main():
 
     st.martkdown("# Hello World")
     st.write(np.__version__)
+    st.write(pd.__version__)
 
     if __name__ == "__main__":
         main()
