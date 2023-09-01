9e5c78dcccd749b4f7d0741d3dc96925cfbfba65
Sol LEE <datasoling@gmail.com>
2023-09-01 10:37:52 +0900

    upup

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
 # -*- coding:utf-8 -*-
 
 import streamlit as st
+from eda_app import run_eda_app
 
 def main():
 
-    st.markdown("# Hello World")
+    st.markdown("Hello World")
     menu = ["Home", "탐색적 자료 분석", "머신러닝", "About"]
     choice = st.sidebar.selectbox("메뉴", menu)
+
     if choice == "Home":
-          st.subheader("Home")
+        st.subheader("Home")
     elif choice == "탐색적 자료 분석":
-          st.subheader("탐색적 자료 분석")
+        run_eda_app()
     elif choice == "머신러닝":
-          st.subheader("머신러닝")
+        st.subheader("머신러닝")
     elif choice == "About":
-          st.subheader("About")
-    else: 
-          pass
+        st.subheader("About")
+    else:
+        pass
 
 
 if __name__ == "__main__":
